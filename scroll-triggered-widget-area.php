aee35a280daaf144392cddd0555b4afad7094c8b
Logesh Kumar <phoenix.logu@gmail.com>
2014-01-25 11:05:38 +0530

    Update scroll-triggered-widget-area.php

--- scroll-triggered-widget-area.php
+++ scroll-triggered-widget-area.php
@@ -9,7 +9,7 @@
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  */
-// If this file is called directly, abort.
+
 if ( ! defined( 'WPINC' ) ) {
 	die;
 }
